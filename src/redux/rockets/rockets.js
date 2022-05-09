4275432f96080fa9999cd207cae1f3cc8ef70791
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-09 15:55:10 -0600

    Made rockets reducer

--- src/redux/rockets/rockets.js
+++ src/redux/rockets/rockets.js
@@ -1,5 +1,20 @@
 // Actions
+const SAVE = 'spacetravelershub/rockets/SAVE';
 
 // Reducer
+export default function reducer(state = [], action) {
+  switch (action.type) {
+    case SAVE:
+      return action.array;
+    default:
+      return state;
+  }
+}
 
 // Action Creators
+
+export function saveRockets(array) {
+  return () => ({
+    array,
+  });
+}
