9fbb131aaca142a91172ba04cb766ace5807bc31
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-10 11:07:32 -0600

    Made badge appear for reserved rockets

--- src/components/rockets/RocketCard.js
+++ src/components/rockets/RocketCard.js
@@ -5,18 +5,9 @@ import { reserveRocket } from '../../redux/rockets/rockets';
 export default function RocketCard() {
   const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
-  const textArray = rockets.map((rocket) => {
-    if (rocket.reserved) {
-      return 'Cancel Reservation';
-    }
-    return 'Reserve Rocket';
-  });
-  const classArray = rockets.map((rocket) => {
-    if (rocket.reserved) {
-      return 'rocket-cancel';
-    }
-    return 'rocket-reserve';
-  });
+  const textArray = rockets.map((rocket) => (rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'));
+  const classArray = rockets.map((rocket) => (rocket.reserved ? 'rocket-cancel' : 'rocket-reserve'));
+  const badgeArray = rockets.map((rocket) => (rocket.reserved ? 'rocket-badge' : 'default'));
 
   const handleReserve = (i) => {
     const action = reserveRocket(i)();
@@ -29,7 +20,10 @@ export default function RocketCard() {
         <img className="rocket-image" src={rocket.image} alt="rocket" />
         <div className="rocket-info">
           <h2 className="rocket-name">{rocket.name}</h2>
-          <div className="rocket-description">{rocket.description}</div>
+          <div className="rocket-description">
+            <span className={badgeArray[i]}>Reserved</span>
+            {rocket.description}
+          </div>
           <button onClick={() => { handleReserve(i); }} className={classArray[i]} type="button">{textArray[i]}</button>
         </div>
       </div>
