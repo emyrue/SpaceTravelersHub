0ff1233794550969947b162ea0f9a8f069b41565
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-10 10:56:11 -0600

    Reserve function implemented

--- src/components/rockets/RocketCard.js
+++ src/components/rockets/RocketCard.js
@@ -1,16 +1,36 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { reserveRocket } from '../../redux/rockets/rockets';
 
 export default function RocketCard() {
   const rockets = useSelector((state) => state.rockets);
+  const dispatch = useDispatch();
+  const textArray = rockets.map((rocket) => {
+    if (rocket.reserved) {
+      return 'Cancel Reservation';
+    }
+    return 'Reserve Rocket';
+  });
+  const classArray = rockets.map((rocket) => {
+    if (rocket.reserved) {
+      return 'rocket-cancel';
+    }
+    return 'rocket-reserve';
+  });
+
+  const handleReserve = (i) => {
+    const action = reserveRocket(i)();
+    dispatch(action);
+  };
+
   return (
-    rockets.map((rocket) => (
+    rockets.map((rocket, i) => (
       <div className="rocket-card" key={rocket.id}>
         <img className="rocket-image" src={rocket.image} alt="rocket" />
         <div className="rocket-info">
           <h2 className="rocket-name">{rocket.name}</h2>
           <div className="rocket-description">{rocket.description}</div>
-          <button className="rocket-reserve" type="button">Reserve Rocket</button>
+          <button onClick={() => { handleReserve(i); }} className={classArray[i]} type="button">{textArray[i]}</button>
         </div>
       </div>
     ))
