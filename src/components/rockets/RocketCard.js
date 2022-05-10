509fc8cd2edcef6c83c15f139144f4d2b213d12f
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-10 11:21:30 -0600

    Added conditional component

--- src/components/rockets/RocketCard.js
+++ src/components/rockets/RocketCard.js
@@ -7,7 +7,6 @@ export default function RocketCard() {
   const dispatch = useDispatch();
   const textArray = rockets.map((rocket) => (rocket.reserved ? 'Cancel Reservation' : 'Reserve Rocket'));
   const classArray = rockets.map((rocket) => (rocket.reserved ? 'rocket-cancel' : 'rocket-reserve'));
-  const badgeArray = rockets.map((rocket) => (rocket.reserved ? 'rocket-badge' : 'default'));
 
   const handleReserve = (i) => {
     const action = reserveRocket(i)();
@@ -21,7 +20,7 @@ export default function RocketCard() {
         <div className="rocket-info">
           <h2 className="rocket-name">{rocket.name}</h2>
           <div className="rocket-description">
-            <span className={badgeArray[i]}>Reserved</span>
+            {rocket.reserved && (<span className="rocket-badge">Reserved</span>)}
             {rocket.description}
           </div>
           <button onClick={() => { handleReserve(i); }} className={classArray[i]} type="button">{textArray[i]}</button>
