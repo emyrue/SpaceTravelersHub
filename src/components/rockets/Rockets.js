3a82aa4aa1e9416bb04230db52359a30070ebe58
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-11 09:14:10 -0600

    Styling completed

--- src/components/rockets/Rockets.js
+++ src/components/rockets/Rockets.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import RocketCard from './RocketCard';
+import { Container } from 'react-bootstrap';
 import '../../styles/rocket.css';
 
-const Rockets = () => (<RocketCard />);
+const Rockets = () => (<Container><RocketCard /></Container>);
 
 export default Rockets;
