4e7c7113106333e9a333f361f00f7268a9a051ca
emyrue <94935633+emyrue@users.noreply.github.com>
2022-05-11 10:03:19 -0600

    Test rocket action creators

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,57 @@
 import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import rocketReducer, { saveRockets, reserveRocket } from './redux/rockets/rockets.js';
+import store from './redux/configureStore.js';
 import App from './App';
+import icon from '../public/favicon.ico';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders site', () => {
+  const site = render(<Provider store={store}><App /></Provider>);
+  expect(site).toMatchSnapshot();
+});
+
+describe('rocket action creators work', () => {
+  test('save rockets action creator works', () => {
+    const state = [
+      {
+        id: 1,
+        rocket_name: 'rocket',
+        description: 'it\'s a rocket',
+        flickr_images:  [icon],
+      },
+    ];
+    const expectedState = [
+      {
+        id: 1,
+        name: 'rocket',
+        description: 'it\'s a rocket',
+        image: icon,
+        reserved: false,
+      },
+    ];
+    const action = saveRockets(state);
+    expect(rocketReducer(state, action())).toStrictEqual(expectedState);
+  });
+  test('reserve rockets action creator works', () => {
+    const state = [
+      {
+        id: 1,
+        name: 'rocket',
+        description: 'it\'s a rocket',
+        image: icon,
+        reserved: false,
+      },
+    ];
+    const expectedState = [
+      {
+        id: 1,
+        name: 'rocket',
+        description: 'it\'s a rocket',
+        image: icon,
+        reserved: true,
+      },
+    ];
+    const action = reserveRocket(1);
+    expect(rocketReducer(state, action())).toStrictEqual(expectedState);
+  });
 });
